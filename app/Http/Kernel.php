04535ae6de97ae00d004e316f4dfa73302748750
AbdulrhmanSalah22 <abdulrhman.salah22@gmail.com>
2022-03-09 16:40:56 +0200

    Adding CORS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        // \App\Http\Middleware\AdminMiddleware::class ,
+         \App\Http\Middleware\AdminMiddleware::class ,
+        \App\Http\Middleware\CORS::class,
     ];
 
     /**
@@ -67,5 +68,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'cors' => \App\Http\Middleware\CORS::class
     ];
 }
