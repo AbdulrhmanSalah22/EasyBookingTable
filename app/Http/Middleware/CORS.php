0c4e12823da49d205ef993cf5546a21ec7abff4d
AbdulrhmanSalah22 <abdulrhman.salah22@gmail.com>
2022-03-09 18:25:18 +0200

    CORS modifications

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -18,6 +18,7 @@ class CORS
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'))
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
