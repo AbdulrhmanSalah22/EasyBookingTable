7bb26b080a8e68dcc855b0db5c28e6de525be4c3
AbdulrhmanSalah22 <abdulrhman.salah22@gmail.com>
2022-03-09 16:49:16 +0200

    Adding Cors in routeServiceProvider For api

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,7 +38,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware(['api','cors'])
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
